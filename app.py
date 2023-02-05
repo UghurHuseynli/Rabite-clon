1121d6913344c346e185d10a6ad847320c53f885
Ugur <hugur2277@gmail.com>
2023-02-05 21:02:34 +0400

    image change

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
-from extensions import *
-from models import *
-from url import *
-from forms import *
+
 from flask import Flask
 
 app = Flask(__name__, template_folder='./templates', static_folder='./static')
@@ -10,6 +7,10 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = 'windwalker_key'
 app.config['FLASK_ADMIN_SWATCH'] = 'Slate'
 
+from extensions import *
+from models import *
+from forms import *
+from url import *
 
 if __name__ == '__main__':
     app.run(debug=True)
